acd649c9212c6e04a90f38d70a8c89c84f0cca68
ersin98 <ersinyurtseven98@gmail.com>
2022-12-06 23:43:46 +0300

    first step for database

---
+++ retrofitDemo/src/test/java/com/ersin/retrofitDemo/RetrofitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ersin.retrofitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
